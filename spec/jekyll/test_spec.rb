73bcaf1098d702aabb0d45bb99dc68f6723f1c7b
James Smith <james@floppy.org.uk>
2017-08-17 12:17:01 +0100

    remove failing autogenerated test

--- spec/jekyll/test_spec.rb
+++ spec/jekyll/test_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Jekyll::Test do
   it "has a version number" do
     expect(Jekyll::Test::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
