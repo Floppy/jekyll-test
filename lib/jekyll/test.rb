4f06dfbae43cd7b6e26e85e40e8cc189acc09da5
James Smith <james@floppy.org.uk>
2017-08-17 12:13:04 +0100

    basic gem framework

---
+++ lib/jekyll/test.rb
@@ -0,0 +1,7 @@
+require "jekyll/test/version"
+
+module Jekyll
+  module Test
+    # Your code goes here...
+  end
+end
---
+++ lib/jekyll/test/version.rb
@@ -0,0 +1,5 @@
+module Jekyll
+  module Test
+    VERSION = "0.1.0"
+  end
+end
